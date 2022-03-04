84bc4adc2ee7a9c74835c32a00162be7329f8377
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-03-04 14:09:07 -0500

    commit tree d10982a1a035c3676611436bf5c710e364af29f6

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses string
+    stack = Stack.new
+
+    string.chars.each do |c|
+        # Check to see if last stack is open "(" && c is closed ")"
+        # If true, remove the last stack
+        if stack.peek == '(' && c == ')' then stack.pop
+        # If false, add c to stack
+        else stack.push(c)
+        end
+    end
+    # stack includes parentheses with missing pairs, so find length to see how many is needed
+    stack.size
+end
+
+
